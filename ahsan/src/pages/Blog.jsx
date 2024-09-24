6654d10159b421a3701f481938d0bdb2f0454184
AhsanSarfraz-2002 <ahsansarfraz135790@gmail.com>
2024-09-24 22:08:01 +0500

    Added award section in blog page

--- ahsan/src/pages/Blog.jsx
+++ ahsan/src/pages/Blog.jsx
@@ -4,12 +4,15 @@ import React from 'react';
 import HeaderBlog from '../components/HeaderBlog';
 import Footer2 from '../components/Footer2';
 import BlogImages from '../components/BlogImages';
+import Extra from '../components/Extra';
 
 const Blog = () => {
   return (
     <div>
 
         <HeaderBlog/>
+
+      <Extra/>
 
       <BlogImages/>
 
